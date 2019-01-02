e1c20aef3cc1a8360b43d6d55feaa0853f69d25b
Mike Simmonds <msimmonds@zonedigital.com>
2019-01-02 16:01:27 +0000

    Fixes #27 - updates browserslist dependency and rewriting test to check for thrown errors rather than snapshot content

--- packages/browserslist-config/index.test.js
+++ packages/browserslist-config/index.test.js
@@ -7,12 +7,10 @@ describe(NPM_PACKAGE, () => {
     expect(Array.isArray(config)).toBe(true)
   })
 
-  it('should not contain invalid queries', () => {
+  it('should not throw an error when extended', () => {
     // eslint-disable-next-line global-require
     jest.doMock(NPM_PACKAGE, () => require('./'), { virtual: true })
 
-    const result = browserslist([`extends ${NPM_PACKAGE}`])
-
-    expect(result).toMatchSnapshot()
+    expect(() => browserslist([`extends ${NPM_PACKAGE}`])).not.toThrow()
   })
 })
