870509bf26e1da6f2d6c757ad190d8aae1344a2b
Mike Simmonds <msimmonds@zonedigital.com>
2019-02-13 08:48:40 +0000

    Fixes #50 - Simplify config packages and moving to explicit NPM package contents

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -1,1 +1,7 @@
-module.exports = require('./config')
+module.exports = {
+  extends: ['plugin:react/recommended', '@zonedigital/eslint-config-zone'],
+  plugins: ['react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+  },
+}
--- packages/eslint-config-zone/index.js
+++ packages/eslint-config-zone/index.js
@@ -1,1 +1,19 @@
-module.exports = require('./config')
+module.exports = {
+  extends: 'airbnb-base',
+  env: {
+    browser: true,
+  },
+  rules: {
+    'brace-style': ['error', '1tbs', { allowSingleLine: false }],
+    'compat/compat': 'warn',
+    complexity: ['error', 10],
+    curly: ['error', 'all'],
+    'operator-linebreak': ['error', 'after'],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: ['const', 'let'], next: '*' },
+      { blankLine: 'any', prev: ['const', 'let'], next: ['const', 'let'] },
+    ],
+  },
+  plugins: ['compat'],
+}
