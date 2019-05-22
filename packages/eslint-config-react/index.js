111268f99faf07ad707ad1e1bc592ea02b73911d
Thomas Jeatt <tjeatt@zonedigital.com>
2019-05-22 14:32:28 +0100

    Add eslint-plugin-jsx-a11y and config changes. This adds accessibility tests to JSX files.

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  extends: ['plugin:react/recommended', '@zonedigital/eslint-config-zone'],
-  plugins: ['react-hooks'],
+  extends: ['plugin:react/recommended', 'plugin:jsx-a11y/recommended', '@zonedigital/eslint-config-zone'],
+  plugins: ['react-hooks', 'jsx-a11y'],
   rules: {
     'react-hooks/exhaustive-deps': 'warn',
     'react-hooks/rules-of-hooks': 'error',
