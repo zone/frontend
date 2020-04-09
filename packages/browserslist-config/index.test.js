f3b189c1cb24ea3c5cdfe3fe61a92441d34d957c
Mike Simmonds <msimmonds@zonedigital.com>
2020-04-09 13:57:54 +0100

    Adding repo wide lint script + fixing issues

--- packages/browserslist-config/index.test.js
+++ packages/browserslist-config/index.test.js
@@ -1,6 +1,6 @@
 const browserslist = require('browserslist')
 const { name: NPM_PACKAGE } = require('./package.json')
-const config = require('./')
+const config = require('.')
 
 describe(NPM_PACKAGE, () => {
   it('exports an array', () => {
@@ -9,7 +9,7 @@ describe(NPM_PACKAGE, () => {
 
   it('does not throw an error when extended', () => {
     // eslint-disable-next-line global-require
-    jest.doMock(NPM_PACKAGE, () => require('./'), { virtual: true })
+    jest.doMock(NPM_PACKAGE, () => require('.'), { virtual: true })
 
     expect(() => browserslist([`extends ${NPM_PACKAGE}`])).not.toThrow()
   })
